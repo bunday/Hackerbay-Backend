3356eb6af0c77bdef8a05e3a22318209b808b4a7
Zadat Olayinka <bundayy@outlook.com>
2020-05-06 11:05:25 +0100

    validate login data

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,11 +1,27 @@
 const express = require("express");
-
+var payloadChecker = require("payload-validator");
+var expectedPayload = {
+  username: "",
+  password: "",
+};
 const router = express.Router();
 
-router.post('',(req, res) => {
+router.post("", (req, res) => {
+  var result = payloadChecker.validator(
+    req.body,
+    expectedPayload,
+    ["username", "password"],
+    false
+  );
+  if (result.success) {
     res.status(200).json({
-        message: 'Login Successful'
-    })
-})
+      message: "Login Successful",
+    });
+  } else {
+    res.status(400).json({
+      message: "Username and Password is required",
+    });
+  }
+});
 
 module.exports = router;
